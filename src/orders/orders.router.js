d407cbcba810c74ace115f495c86242472c40ae2
Vincent Mills <vincentmills273@gmail.com>
2022-04-16 00:31:53 -0400

    Removed generic controller class
    The controller class was getting a little unwieldy, so I removed it and reverted the /orders controller and router back to their original state.

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,16 +1,5 @@
 const router = require("express").Router();
-const orderController = require("./orders.controller")
-const methodNotAllowed = require("../errors/methodNotAllowed")
+
 // TODO: Implement the /orders routes needed to make the tests pass
-
-router.route("/").get(orderController.list)
-    .post(orderController.create)
-    .all(methodNotAllowed)
-
-router.route("/:orderId")
-    .get(orderController.read)
-    .put(orderController.update)
-    .delete(orderController.delete)
-    .all(methodNotAllowed)
 
 module.exports = router;
